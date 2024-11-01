34a8ed949608a373d879dc750e8f5665d5d7352b
Naresh-Kushwaha <136280624+Naresh-Kushwaha@users.noreply.github.com>
2024-11-01 21:18:42 +0530

    UserServiceAdded

---
+++ userservice/src/main/java/com/mithril/chatapp/userservice/enums/CountryCode.java
@@ -0,0 +1,13 @@
+package com.mithril.chatapp.userservice.enums;
+public enum CountryCode {
+    US("+1"),
+    IN("+91"),
+    UK("+44");
+    private final String code;
+    CountryCode(String code) {
+        this.code = code;
+    }
+    public String getCode() {
+        return code;
+    }
+}
---
+++ userservice/src/test/java/com/mithril/chatapp/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithril.chatapp.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
