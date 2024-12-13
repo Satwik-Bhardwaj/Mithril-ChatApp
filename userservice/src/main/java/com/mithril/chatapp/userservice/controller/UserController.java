1a61b587b645d39674a51b95b9c12467ae9eb2c2
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2024-12-13 21:58:28 +0530

    9-add-swagger-for-api-documentation added feature

--- userservice/src/main/java/com/mithril/chatapp/userservice/controller/UserController.java
+++ userservice/src/main/java/com/mithril/chatapp/userservice/controller/UserController.java
@@ -3,11 +3,14 @@ package com.mithril.chatapp.userservice.controller;
 import com.mithril.chatapp.userservice.entity.Users;
 import com.mithril.chatapp.userservice.service.UserService;
 import com.mithril.chatapp.userservice.enums.CountryCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
+@Tag(name = "User Controller", description = "API for managing users")
 @RestController
 @RequestMapping("/api")
 public class UserController {
@@ -15,11 +18,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create user", description = "Create a new user using this POST endpoint")
     @PostMapping("/create")
     public ResponseEntity<?>createUser(@RequestBody Users users){
         return userService.createUser(users);
     }
 
+    @Operation(summary = "Get user by phone", description = "Get the user details by their phone number")
     @GetMapping("/getByPhoneNumber")
     public Users getUserByPhoneNumberAndCountryCode(
             @RequestParam Long phoneNumber,
@@ -27,19 +32,21 @@ public class UserController {
         return userService.getUserByPhoneNumber(phoneNumber,countryCode);
     }
 
+    @Operation(summary = "Get user by ID", description = "Get the user details by their userID")
     @GetMapping("/getById")
     public Users getUserById(@RequestParam UUID id){
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "Delete user by phone", description = "Delete the user record from DB by their phone number")
     @DeleteMapping("/deleteByPhoneNumber")
     public ResponseEntity<Void> deleteUserByPhoneNumberAndCountryCode(@RequestParam Long phoneNumber, @RequestParam CountryCode countryCode){
         return userService.deleteUserByPhoneNumber(phoneNumber,countryCode);
     }
 
+    @Operation(summary = "Delete user by ID", description = "Delete the user record from DB by their userID")
     @DeleteMapping("/deleteById")
     public ResponseEntity<Void> deleteUserById(@RequestParam UUID id){
         return userService.deleteUserById(id);
     }
-
 }
--- userservice/src/main/java/com/mithril/chatapp/userservice/entity/Users.java
+++ userservice/src/main/java/com/mithril/chatapp/userservice/entity/Users.java
@@ -1,8 +1,10 @@
 package com.mithril.chatapp.userservice.entity;
 import com.mithril.chatapp.userservice.enums.CountryCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Schema(name = "Users", description = "Represents a User")
 @Entity
 @Getter
 @Setter
@@ -10,8 +12,10 @@ public class Users extends BaseEntity {
 
     private String username;
 
+    @Schema(description = "Users' Profile Bio")
     private String bio;
 
+    @Schema(description = "Country Code of the Phone number", example = "'US', 'IN' or 'UK', etc.")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private CountryCode countryCode;
