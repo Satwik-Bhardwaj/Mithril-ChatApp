9b040e8a4e939ec6a79fcb6259b3444bbd04176d
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2026-03-09 23:12:27 +0530

    creating a websocket gateway for chat app

---
+++ wsservice/wsservice/src/main/java/com/mithril/wsg/wsservice/WsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mithril.wsg.wsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsserviceApplication.class, args);
+	}
+
+}
---
+++ wsservice/wsservice/src/test/java/com/mithril/wsg/wsservice/WsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithril.wsg.wsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
